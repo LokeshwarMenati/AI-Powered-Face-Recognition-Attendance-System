8c2ded2f652e5ec40f569370d1d5bbe5fe86dfb2
LokeshwarMenati <lokeshwarmenati@gmail.com>
2026-09-23 14:04:55 +0530

    Fix sys.path in wsgi.py for Vercel serverless import

--- Project-Face-attandence-system-version-1.0/Project101/wsgi.py
+++ Project-Face-attandence-system-version-1.0/Project101/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
+
+CURRENT_DIR = Path(__file__).resolve().parent
+PROJECT_DIR = CURRENT_DIR.parent
+if str(PROJECT_DIR) not in sys.path:
+    sys.path.insert(0, str(PROJECT_DIR))
 
 from django.core.wsgi import get_wsgi_application
 
